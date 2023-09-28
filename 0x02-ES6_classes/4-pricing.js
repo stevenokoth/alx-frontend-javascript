6f50c7181d8420e443a767c13f1ff537c1f346a3
stevenokoth <stevenocholao@gmail.com>
2023-09-28 22:24:25 +0300

    java

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,15 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
